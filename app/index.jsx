bdd2fc0d0df4211609513dec517cba01dbd9b4d0
scarletrunner7000 <scarletrunner7000@gmail.com>
2017-09-24 22:06:26 +0900

    pass yarn run build-renderer

---
+++ app/index.jsx
@@ -0,0 +1,26 @@
+import ReactDom from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import Root from './containers/Root';
+import { configureStore, history } from './store/configureStore';
+import './app.global.css';
+
+const store = configureStore();
+
+const render = (component) => {
+  ReactDom.render(
+    <AppContainer>
+      {component}
+    </AppContainer>,
+    document.getElementById('root')
+  );
+}
+
+render(<Root store={store} history={history} />)
+
+
+if (module.hot) {
+  module.hot.accept('./containers/Root', () => {
+    const NextRoot = require('./containers/Root'); // eslint-disable-line global-require
+    render(<NextRoot store={store} history={history} />)
+  });
+}
---
+++ app/routes.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Switch, Route } from 'react-router';
+import App from './containers/App';
+
+export default () => (
+  <App>
+    <Switch>
+      <Route path="/" component={<span>hogehoge</span>} />
+    </Switch>
+  </App>
+);
