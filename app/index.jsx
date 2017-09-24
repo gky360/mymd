6dc836fd4c0eb14a57d040ee8beb3e53ab251e56
scarletrunner7000 <scarletrunner7000@gmail.com>
2017-09-25 00:19:09 +0900

    pass yarn run start-main-dev

--- app/index.jsx
+++ app/index.jsx
@@ -1,26 +1,28 @@
-import ReactDOM from 'react-dom';
+import React from 'react';
+import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import Root from './containers/Root';
 import { configureStore, history } from './store/configureStore';
 import './app.global.css';
 
+console.log(configureStore, history);
 const store = configureStore();
 
-const render = (component) => {
-  ReactDOM.render(
-    <AppContainer>
-      {component}
-    </AppContainer>,
-    document.getElementById('root')
-  );
-}
-
-render(<Root store={store} history={history} />)
-
+render(
+  <AppContainer>
+    <Root store={store} history={history} />
+  </AppContainer>,
+  document.getElementById('root')
+);
 
 if (module.hot) {
   module.hot.accept('./containers/Root', () => {
     const NextRoot = require('./containers/Root'); // eslint-disable-line global-require
-    render(<NextRoot store={store} history={history} />)
+    render(
+      <AppContainer>
+        <NextRoot store={store} history={history} />
+      </AppContainer>,
+      document.getElementById('root')
+    );
   });
 }
