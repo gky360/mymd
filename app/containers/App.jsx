c78be4bf2800130ccb3b4c8cdf5fa4f1d5775d6f
scarletrunner7000 <scarletrunner7000@gmail.com>
2017-09-25 00:38:41 +0900

    pass yarn run flow

--- app/containers/App.jsx
+++ app/containers/App.jsx
@@ -1,12 +1,13 @@
 // @flow
 import React, { Component } from 'react';
-import type { Children } from 'react';
+import type { Element } from 'react';
 
-export default class App extends Component {
-  props: {
-    children: Children
-  };
 
+type Props = {
+  children: Element<any>
+}
+
+export default class App extends Component<Props> {
   render() {
     return (
       <div>
